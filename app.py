bbd082cfcf850801dcbde5b0306bef7adafa31dd
Ankit <ankitbohare16@gmail.com>
2026-05-26 20:04:21 +0530

    working

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 import streamlit as st
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 # import time
 from src.helper import get_pdf_text, get_text_chunks, get_vector_store, get_conversational_chain
+
+
+def user_input(user_quesiton):
+        response = st.session_state.conversation({"question": user_quesiton})
+        st.session_state.chat_history = response["chat_history"]
+        for i, message in enumerate(st.session_state.chat_history):
+            if i % 2 == 0:
+                st.markdown(f"**User:** {message.content}")
+            else:
+                st.markdown(f"**Reply:** {message.content}")
+
 
 def main():
     load_dotenv()
     st.set_page_config("DORA")
     st.header("Information Retrieval System")
+    
+    user_quesiton = st.text_input("Ask a question about your documents here:")
+    
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+    if user_quesiton:
+        user_input(user_quesiton)
+        
+            
+            
     
     with st.sidebar:
         st.title("Menu:")
--- src/helper.py
+++ src/helper.py
@@ -4,39 +4,72 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
+# from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 
 load_dotenv()
+
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 
+
 def get_pdf_text(pdf_docs):
     text = ""
+
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
+
         for page in pdf_reader.pages:
             text += page.extract_text()
+
     return text
 
 
 def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=20
+    )
+
     chunks = text_splitter.split_text(text)
+
     return chunks
 
 
 def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+    vectorstore = FAISS.from_texts(
+        texts=text_chunks,
+        embedding=embeddings
+    )
+
     return vectorstore
 
 
 def get_conversational_chain(vectorstore):
-    llm = ChatGoogleGenerativeAI(model="gemini-pro")
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vectorstore.as_retriever(), memory=memory)
+
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-1.5-flash",
+        temperature=0.3,
+        convert_system_message_to_human=True
+    )
+
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True
+    )
+
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+
     return conversation_chain
